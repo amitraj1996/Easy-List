39b9d94a4b57a4f558144601fd48e93b3362c6fa
Raj Amit Singh <raj.singh@vvdntech.in>
2019-02-17 01:14:57 +0530

    Imporving UI

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 
-
-class ProductPage extends StatelessWidget{
+class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    //
     return Scaffold(
       appBar: AppBar(
         title: Text('Product Details'),
       ),
-      body: Column(children: <Widget>[
-        Text('Details'),
-        RaisedButton(child: Text('BACK'),
-        onPressed: ()=> Navigator.pop(context),
-        )
-      ],),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/food2.jpg'),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('Details !'),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('BACK'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
-} 
+}
