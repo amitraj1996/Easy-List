6d41451a0bd8ccfe63a5d838b87288341fc822c6
Raj Amit Singh <raj.singh@vvdntech.in>
2019-03-03 01:34:36 +0530

    added dialog box

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -11,49 +11,40 @@ class ProductsAdminPage extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        drawer: Drawer(
-          child: Column(
-            children: <Widget>[
-              AppBar(
-                automaticallyImplyLeading: false,
-                title: Text('Choose'),
-              ),
-              ListTile(
-                title: Text('All Products'),
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => ProductsPage()
-                    )
-                    );
-                },
-              )
-            ],
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text('Choose'),
+                ),
+                ListTile(
+                  title: Text('All Products'),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(context, '/');
+                  },
+                )
+              ],
+            ),
           ),
-        ),
-        appBar: AppBar(
-          title: Text('Manage Products'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.create),
-                text: 'Create Product',
-              ),
-              Tab(
-                icon: Icon(Icons.list),
-                text: 'My Products',
-              )
-            ],
+          appBar: AppBar(
+            title: Text('Manage Products'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.create),
+                  text: 'Create Product',
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                  text: 'My Products',
+                )
+              ],
+            ),
           ),
-        ),
-          body:TabBarView(
-            children: <Widget>[
-              ProductCreatePage(),
-              ProductListPage()
-            ],
-          )
-      ),
+          body: TabBarView(
+            children: <Widget>[ProductCreatePage(), ProductListPage()],
+          )),
     );
   }
 }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,59 +2,23 @@ import 'package:flutter/material.dart';
 import './products.dart';
 import './product_control.dart';
 
-class ProductManager extends StatefulWidget {
-  final Map<String, String> startingProduct;
+class ProductManager extends StatelessWidget {
 
-  ProductManager({this.startingProduct}) {
-    print('[productManager state] constructor');
-  }
+final List<Map<String,String>> products;
+final Function addProduct;
+final Function deleteProduct;
 
-  @override
-  State<StatefulWidget> createState() {
-    print('[productManager state] createstate()');
-    return _ProductManagerState();
-  }
-}
-
-class _ProductManagerState extends State<ProductManager> {
-  List<Map<String, String>> _products = [];
-
-  @override
-  void initState() {
-    print('[productManager state] initState()');
-    if(widget.startingProduct != null){
-    _products.add(widget.startingProduct);
-    }
-    super.initState();
-  }
-
-  @override
-  void didUpdateWidget(ProductManager oldWidget) {
-    print('[productManager state] didUpdateWidget()');
-    super.didUpdateWidget(oldWidget);
-  }
-
-  void _addProduct(Map<String, String> product) {
-    setState(() {
-      _products.add(product);
-    });
-  }
-
-  void _deleteProduct(int index) {
-    setState(() {
-      _products.removeAt(index);
-    });
-  }
-
+ProductManager(this.products, this.addProduct, this.deleteProduct);
+ 
   @override
   Widget build(BuildContext context) {
     print('[productManager state] build');
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10.0),
-        child: ProductControl(_addProduct),
+        child: ProductControl(addProduct),
       ),
-      Expanded(child:Products(_products, deleteProduct:_deleteProduct))
+      Expanded(child:Products(products, deleteProduct:deleteProduct))
     ]);
   }
 }
